6bf3d7f2e6bb2508d9de299a019c85ff9ad637e4
Ron Fallara <rfallara@fallara.net>
2018-12-27 21:16:09 -0500

    Trips working in Material Table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {RoomsService} from './room-management/rooms.service';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { RoomsService} from './room-management/rooms.service';
+import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { ResortComponent } from './room-management/resort/resort.component';
 import { RoomTypeComponent } from './room-management/room-type/room-type.component';
 import { RoomManagementComponent } from './room-management/room-management.component';
-import {AuthService} from './authService.service';
+import { AuthService } from './authService.service';
 import { LoginComponent } from './login/login.component';
-import {FormsModule} from '@angular/forms';
-import {AuthInterceptor} from './auth-interceptor.service';
-import {AppRoutingModule} from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './auth-interceptor.service';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { TripManagementComponent } from './trip-management/trip-management.component';
 import { PointManagementComponent } from './point-management/point-management.component';
-import {Globals} from './gobals';
-import {TripsService} from './trip-management/trips.service';
+import { Globals } from './gobals';
+import { TripsService } from './trip-management/trips.service';
 import { AddTripComponent } from './trip-management/add-trip/add-trip.component';
-import {NgxSpinnerModule} from 'ngx-spinner';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -36,11 +46,13 @@ import {NgxSpinnerModule} from 'ngx-spinner';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
     NgbModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule
   ],
   exports: [FormsModule],
   providers: [
--- src/app/trip-management/trip-management.component.ts
+++ src/app/trip-management/trip-management.component.ts
@@ -1,20 +1,27 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {TripsService} from './trips.service';
 import {Subscription} from 'rxjs';
 import {Trip} from './trip.model';
 import {NgxSpinnerService} from 'ngx-spinner';
 import {AddTripComponent} from './add-trip/add-trip.component';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {BookableRoom} from '../room-management/bookable-room.model';
+// import {BookableRoom} from '../room-management/bookable-room.model';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-trip-management',
   templateUrl: './trip-management.component.html',
   styleUrls: ['./trip-management.component.css']
 })
-export class TripManagementComponent implements OnInit, OnDestroy {
+export class TripManagementComponent implements OnInit, OnDestroy, AfterViewInit {
   private tripsSubscription: Subscription;
   public trips: Trip[];
+  displayedColumns: string[] = ['booked_date', 'check_in_date', 'check_out_date',
+    'owner', 'resort', 'room_type', 'notes', 'points', 'delete'];
+  dataSource =  new MatTableDataSource<Trip>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sorter: MatSort;
+
   isLoading = true;
 
   constructor(private tripsService: TripsService,
@@ -27,6 +34,7 @@ export class TripManagementComponent implements OnInit, OnDestroy {
     this.tripsSubscription = this.tripsService.tripsChanged.subscribe(
       (trips: Trip[]) => {
         this.trips = trips;
+        this.dataSource.data = trips;
         this.spinner.hide();
         this.isLoading = false;
       },
@@ -36,10 +44,37 @@ export class TripManagementComponent implements OnInit, OnDestroy {
         this.isLoading = false;
       }
     );
+
+    this.dataSource.sortingDataAccessor = (item, property) => {
+      switch (property) {
+        case 'owner' : return item.owner.name;
+        default: return item[property];
+      }
+    };
+
+    this.dataSource.filterPredicate = (data: Trip, filter: string) => {
+      const searchString = data.owner.name + data.bookable_room.resort.name + data.bookable_room.room_type.name +
+        data.notes + data.points_needed.toString();
+
+      if (searchString.toLowerCase().search(filter) >= 0) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sorter;
   }
 
   ngOnDestroy() {
     this.tripsSubscription.unsubscribe();
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   onAddNewTrip() {
