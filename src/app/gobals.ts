55aa37fe16d88830f0d57f831bfeb7a1bd63fdf8
Ron Fallara <rofallar@cisco.com>
2019-08-03 00:04:34 -0400

    cleanup points styling

--- src/app/gobals.ts
+++ src/app/gobals.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Globals {
-  dvcApiServer = 'http://127.0.0.1:5000';
+  // dvcApiServer = 'http://127.0.0.1:5000';
+  dvcApiServer = 'https://dvc.fallara.net';
   // dvcApiServer = 'https://dvc-dev.fallara.net';
 }
