d8760b22af39714b330917eec260f658397c94ff
Ron Fallara <rofallar@cisco.com>
2020-02-04 17:44:55 -0500

    converted to reactive form

--- src/app/trip-management/add-trip/add-trip.component.ts
+++ src/app/trip-management/add-trip/add-trip.component.ts
@@ -4,7 +4,7 @@ import {TripsService} from '../trips.service';
 import {BookableRoom} from '../../room-management/bookable-room.model';
 import {Subscription} from 'rxjs';
 import {Owner} from '../../shared/owner.model';
-import {NgForm} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {Resort} from '../../room-management/resort.model';
 
 @Component({
@@ -13,6 +13,9 @@ import {Resort} from '../../room-management/resort.model';
   styleUrls: ['./add-trip.component.css']
 })
 export class AddTripComponent implements OnInit, OnDestroy {
+  addTripForm: FormGroup;
+  bookableRoomRoom: FormControl;
+
   public resorts: Resort[];
   private resortsSubscription: Subscription;
   public bookableRooms: BookableRoom[];
@@ -27,6 +30,22 @@ export class AddTripComponent implements OnInit, OnDestroy {
   constructor(public activeModal: NgbActiveModal, private tripsService: TripsService) {}
 
   ngOnInit() {
+
+    this.bookableRoomRoom = new FormControl(null);
+
+    this.addTripForm = new FormGroup({
+      'booked_date': new FormControl(null),
+      'points_needed': new FormControl(null),
+      'check_in_date': new FormControl(null),
+      'check_out_date': new FormControl(null),
+      'owner': new FormControl({value: null, disabled: this.ownersLoading}),
+      'bookable_room_resort': new FormControl({value: null, disabled: this.resortsLoading || this.bookableRoomsLoading}),
+      'bookable_room_room': new FormControl({value: null, disabled: this.resortsLoading || this.bookableRoomsLoading}),
+      'notes': new FormControl(null)
+    });
+
+
+
     this.ownersLoading = true;
     this.tripsService.getOwners();
     this.ownersSubscription = this.tripsService.ownersChanged.subscribe(
@@ -42,7 +61,7 @@ export class AddTripComponent implements OnInit, OnDestroy {
         this.resorts = resorts;
         this.resortsLoading = false;
       }
-    )
+    );
     this.bookableRoomsLoading = true;
     this.tripsService.getBookableRooms();
     this.bookableRoomSubscription = this.tripsService.bookableRoomsChanged.subscribe(
@@ -60,35 +79,33 @@ export class AddTripComponent implements OnInit, OnDestroy {
   }
 
   onResortChange(resort) {
-    const selectedResort = <HTMLOptionElement>resort[resort.selectedIndex];
     this.bookableRoomsFiltered = this.bookableRooms.filter(
-      bookableRoom => bookableRoom.resort.name === selectedResort.label);
-    // this.bookableRoomsFiltered.filter(br => br.resort.name === selectedResort.label);
+      bookableRoom => bookableRoom.resort.name === resort.value);
   }
 
-  onAddTrip(form) {
-    const values = <NgForm>form.value;
+  onAddTrip() {
     const newTrip = {
-      'booked_date': new Date(values['booked_date']),
-      'points_needed': values['points_needed'],
-      'notes': values['notes'],
+      'booked_date': new Date(this.addTripForm.get('booked_date').value),
+      'points_needed': this.addTripForm.get('points_needed').value,
+      'notes': this.addTripForm.get('notes').value,
       'owner': {
-        'name': values['owner']
+        'name': this.addTripForm.get('owner').value
       },
-      'check_in_date': new Date(values['check_in_date']),
-      'check_out_date': new Date(values['check_out_date']),
+      'check_in_date': new Date(this.addTripForm.get('check_in_date').value),
+      'check_out_date': new Date(this.addTripForm.get('check_out_date').value),
       'bookable_room': {
         'room_type': {
-          'name': values['bookable_room_room']
+          'name': this.addTripForm.get('bookable_room_room').value
         },
         'resort': {
-          'name': values['bookable_room_resort']
+          'name': this.addTripForm.get('bookable_room_resort').value
         }
       }
     }
     console.log(newTrip);
     this.tripsService.addTrip(newTrip);
     this.activeModal.close('tripAdded');
+    console.log(this.addTripForm);
   }
 
 }
