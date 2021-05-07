8060e652c02a3755e1f40c0ab966c0b6cd0a33fc
Ron Fallara <rofallar@cisco.com>
2021-05-07 15:20:19 -0400

    Moved api server to environment variable

--- src/app/gobals.ts
+++ src/app/gobals.ts
@@ -1,8 +1,13 @@
-import {Injectable} from '@angular/core';
+import { environment } from './../environments/environment';
+import { Injectable } from '@angular/core';
+
 
 @Injectable()
 export class Globals {
+  dvcApiServer = environment.dvcApiServer
   // dvcApiServer = 'http://127.0.0.1:5000';
+  // dvcApiServer = 'http://127.0.0.1:8000'; // gunicorn
   // dvcApiServer = 'https://dvc.fallara.net';
-  dvcApiServer = 'https://dvc-dev.fallara.net';
+  // dvcApiServer = 'https://dvc-dev.fallara.net';
+  // dvcApiServer = 'https://dvc-stage.fallara.net';
 }
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  dvcApiServer: 'https://dvc.fallara.net'
+  dvcApiServer: 'https://dvc-dev.fallara.net'
 };
